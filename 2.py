5c638d3f89e0e250767c9607ba29b4b81993ba2e
soipachara43 <61070235@kmitl.ac.th>
2018-11-29 13:02:03 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -2,12 +2,6 @@
 def main():
     """ II """
     num = int(input())
-    count = 0
-    for j in range(num, 0, -1):
-        print(j, end=' ')
-        count += 1
-        if count == 7:
-            print(' ')
-            count = 0
+
 
 main()
