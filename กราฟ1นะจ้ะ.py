fb323d4b8c761800dac205ffe5f5afda277f70bd
soipachara43 <61070235@kmitl.ac.th>
2018-11-30 15:23:01 +0700

    กราฟ1

--- กราฟ1นะจ้ะ.py
+++ กราฟ1นะจ้ะ.py
@@ -11,7 +11,7 @@ def spirit():
     line_graph.add("10-14 years old", data.ttt)
     line_graph.add("15-19 years old", data.rrr)
     line_graph.add("20-24 years old", data.yyy)
-    line_graph.add("26-29 years old", data.iii)
+    line_graph.add("25-29 years old", data.iii)
     line_graph.add("30-34 years old", data.ooo)
     line_graph.add("35-39 years old", data.ppp)
     line_graph.add("Above 40 years old", data.ddd)
